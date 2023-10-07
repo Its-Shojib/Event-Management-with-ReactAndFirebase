beddbbb1eda0f54d29274cd793fa48ddcf6b1c5b
Its_Shojib <mdshojib922@gmail.com>
2023-10-07 21:34:43 +0600

    Layout Section Issue Solved

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -9,19 +9,19 @@ const Home = () => {
     return (
         <div>
             <Banner></Banner>
-            <h2 className="text-3xl font-bold text-center mt-16 mb-5">Explore Our Services</h2>
-            <div className="bg-gradient-to-r from-teal-300 to-purple-400 rounded-lg">
-
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-5 px-4 md:gap-10 md:px-20">
-                    {
-                        items.map(item => <DataCard
-                            key={item.id}
-                            item={item}
-                        ></DataCard>)
-                    }
+            <div>
+                <h2 className="text-3xl font-bold text-center mt-16 mb-5">Explore Our Services</h2>
+                <div className="bg-gradient-to-r from-teal-300 to-purple-400 rounded-lg">
+                    <div className="grid grid-cols-1 md:grid-cols-2 gap-5 px-4 md:gap-10 md:px-20">
+                        {
+                            items.map(item => <DataCard
+                                key={item.id}
+                                item={item}
+                            ></DataCard>)
+                        }
+                    </div>
                 </div>
             </div>
-
             <WhyChooseUs></WhyChooseUs>
         </div>
     );
