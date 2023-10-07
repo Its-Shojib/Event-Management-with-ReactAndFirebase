bf961d868e93c9739f3bd5c8840b52ef96d8e28d
Its_Shojib <mdshojib922@gmail.com>
2023-10-08 02:49:54 +0600

    404 Route Added

--- src/Layout/Error.jsx
+++ src/Layout/Error.jsx
@@ -1,8 +1,15 @@
+import { Link } from "react-router-dom";
 
 const Error = () => {
     return (
-        <div>
-            
+        <div className="min-h-screen flex justify-center items-center">
+            <div className="space-y-5">
+                <img className="w-full md:w-4/6 mx-auto" src="/src/assets/000-404.png" alt="" />
+                <div className="flex justify-center items-center">
+                    <Link className="bg-green-500 text-white px-4 py-2 rounded-md" to='/'>Back to Home</Link>
+                </div>
+            </div>
+
         </div>
     );
 };
