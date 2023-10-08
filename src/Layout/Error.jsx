23c06c06ba3e784d4451d838eb2ef43b2ee9c6af
Its_Shojib <mdshojib922@gmail.com>
2023-10-08 18:40:54 +0600

    Solved Error Route

--- src/Layout/Error.jsx
+++ src/Layout/Error.jsx
@@ -4,7 +4,7 @@ const Error = () => {
     return (
         <div className="min-h-screen flex justify-center items-center">
             <div className="space-y-5">
-                <img className="w-full md:w-4/6 mx-auto" src="/src/assets/000-404.png" alt="" />
+                <img className="w-full md:w-4/6 mx-auto" src="/000-404.png" alt="" />
                 <div className="flex justify-center items-center">
                     <Link className="bg-green-500 text-white px-4 py-2 rounded-md" to='/'>Back to Home</Link>
                 </div>
