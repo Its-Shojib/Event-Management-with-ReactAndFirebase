3cabcc110d549524438b4c6c1c20d1f8d82a4ef1
Its_Shojib <mdshojib922@gmail.com>
2023-10-08 19:03:20 +0600

    Firebase Hiden Successfully

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -6,12 +6,18 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyASFUGKAwn6q_1ADsYlaqpVZiXkG5XkNgI",
-  authDomain: "event-management-e5ed0.firebaseapp.com",
-  projectId: "event-management-e5ed0",
-  storageBucket: "event-management-e5ed0.appspot.com",
-  messagingSenderId: "882043287116",
-  appId: "1:882043287116:web:9f89044bd6a052c14b66e9"
+  // apiKey: "AIzaSyASFUGKAwn6q_1ADsYlaqpVZiXkG5XkNgI",
+  // authDomain: "event-management-e5ed0.firebaseapp.com",
+  // projectId: "event-management-e5ed0",
+  // storageBucket: "event-management-e5ed0.appspot.com",
+  // messagingSenderId: "882043287116",
+  // appId: "1:882043287116:web:9f89044bd6a052c14b66e9"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
